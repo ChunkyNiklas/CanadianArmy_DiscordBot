981d2d41080b1b9e4638e4d88b8d622be83839c1
niklas <hnweiland@we-elektronik.de>
2023-02-18 12:48:33 +0100

    Initial

--- src/main/java/dev/hnweiland/Main.java
+++ src/main/java/dev/hnweiland/Main.java
@@ -3,13 +3,28 @@ package dev.hnweiland;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
    private static JDA jda = null;
-    public static void main(String[] args) {
-        JDABuilder jdaBuilder = JDABuilder.createDefault(args[0]);
+
+   private static BufferedReader bufferedReader;
+
+
+    public Main() throws FileNotFoundException {
+    }
+
+    public static void main(String[] args) throws IOException {
+        bufferedReader = new BufferedReader(new FileReader("src/main/resources/token"));
+        JDABuilder jdaBuilder = JDABuilder.createDefault(bufferedReader.readLine());
         jdaBuilder.setBulkDeleteSplittingEnabled(false);
 
         jda = jdaBuilder.build();
+
+
 
     }
 }
